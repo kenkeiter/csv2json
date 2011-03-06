74eec6893adc465a7f23d406161fb45cee0738c1
kenkeiter <ken@kenkeiter.com>
2011-03-05 21:37:10 -0800

    Added full directory conversion support. Smartly removes blank columns when using --headers. Added --relational output option.

--- csv2json.rb
+++ csv2json.rb
@@ -1,33 +1,31 @@
 require 'json'
 require 'csv'
 require 'optparse'
+require 'pathname'
 
 def convert(file, options)
   row_count, rows, headers = 0, [], []
   CSV.foreach(file) do |row|
-    if options[:use_headers]
-      if row_count == 0
-        headers = row
-        row_count += 1
-        next
-      end
+    if row_count == 0 and options[:use_headers]
+      row.delete_if{|field| field.nil? }
+      headers = row
+      row_count += 1
+      next # skip adding the row as data
     end
     rows << row
     row_count += 1
   end
   
-  puts "Number of rows: #{rows.length} #{file}"
-  
-  if options[:use_headers]
+  if options[:use_headers] and options[:associative_rows]
     output = []
     rows.each do |row|
       item = {}
       headers.size.times { |i| item[ headers[i] ] = row[i] }
       output << item
     end
-    return output.to_json
+    return output.to_json # output objects with k=>v association
   else
-    return rows.to_json
+    return rows.to_json # output an array of arrays (rows)
   end
   
 end
@@ -36,13 +34,22 @@ options = {:conversions => []}
 parser = OptionParser.new do |opts|
   opts.banner = "Usage: ruby csv2json.rb [options]"
   opts.on('-h', '--headers'){|v| options[:use_headers] = v}
+  opts.on('-r', '--relational'){|v| options[:associative_rows] = v}
 end
 parser.order(ARGV) do |file|
-  options[:conversions] << file.split(':')
+  if File.directory?(file)
+    file_set = Dir.glob(File.join(file, '*.csv'))
+    dests = file_set.map do |path|
+      "#{path}.json"
+    end
+    options[:conversions].concat file_set.zip(dests)
+  else
+    options[:conversions] << file.split(':')
+  end
 end
 
 options[:conversions].each do |from, to|
-  puts "Converting #{from} to #{to}"
+  puts "Converting #{from} -> #{to}"
   json_data = convert(from, options)
   fp = File.open(to, 'w+')
   fp.write(json_data)
